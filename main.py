e1bd9cae2022b5bf0ba1499c8699f3d90f3df613
GanbaatarErdene <103094113+GanbaatarErdene@users.noreply.github.com>
2023-08-11 10:52:20 +0800

    main file

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import concurrent.futures
-from ctrip_request import ctrip_req
 from ctrip_sync import request
 from fastapi import FastAPI
-from redis import Redis
-from database.mongodb import cityCollection
-from database.mongodb import hotelsCollection
-from database.mongodb import hotelCollection
-from dotenv import load_dotenv
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
@@ -17,32 +10,50 @@ ctrip = request.HotelDataFetcher()
 def read_root():
     return config['MONGO_DB']
 
+# full sync CityID-г авна.
 @app.get("/city")
 def read_root():
     ctrip.save_city()
 
-@app.get("/hotels")
+# full sync HotelID-г авна.
+@app.get("/hotel")
 def read_root():
     ctrip.save_hotels()
 
-@app.get("/hotel")
+# full sync Hotel Detail-г авна.
+@app.get("/detail")
 def read_root():
     ctrip.get_hotel()
 
+# 24 цагийн өөрчлөлтийг авна.
 @app.get("/change")
 def read_root():
     ctrip.get_changes()
 
-@app.get("/upHotel")
+# 24 цагийн change өөрчлөгдсөн HotelID-р detail-н дуудаж хуучин байга detail дээрээ update хийнэ.
+@app.get("/update")
 def read_root():
     ctrip.update_detail()
 
+# Хоосон ирсэн ResponseStatus.Ack": 'Warning' байга detail-г устгана.
+@app.get("/delete")
+def read_root():
+    ctrip.delete_detail()
 
+# HotelID нь давхардсан үгүйг шалгах.
+@app.get("/same")
+def read_root():
+    ctrip.find_hotels()
+
+# Detail нь давхардсан үгүйг шалгах.
+@app.get("/sameDetail")
+def read_root():
+    ctrip.find_details()
 
 @app.get("/data")
 def read_root():
-    ctrip.get_city()
-    ctrip.get_hotels()
+    ctrip.save_city()
+    ctrip.save_hotels()
     ctrip.get_hotel()
 
 
